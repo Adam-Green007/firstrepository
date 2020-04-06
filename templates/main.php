cb6b758887c8708b552705469fbb51a2d280773c
Adam Green <greengusadam@gmail.com>
2020-04-06 16:42:18 +0300

    шаблонизация

---
+++ templates/main.php
@@ -0,0 +1,39 @@
+
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+    <ul class="promo__list">
+        <?php foreach ($categories as $category): ?>
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="pages/all-lots.html"><?= $category?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
+        <?php foreach ($winterProduct as $key =>$value):?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?= $value['urlpics']; ?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?= $value['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $value['title'];?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?= cost ($value['price']); ?><b class=&#8381>р</b></span>
+                        </div>
+                        <div class="lot__timer timer">
+
+                        </div>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach;?>
+    </ul>
+</section>
